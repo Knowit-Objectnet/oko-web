951dd02a608f8cb679442ebb660962490a80ce27
Tobias Kongsvik <tobias.kongsvik@knowit.no>
2020-06-09 16:00:31 +0200

    update webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,31 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
-    entry: {
-        app: path.join(__dirname, 'src', 'index.tsx')
-    },
-    target: 'web',
+    mode: 'development',
+    devtool: 'source-map',
+
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: '/node_modules/'
-            }
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
         ],
     },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html')
-        })
-    ]
-}
+            template: path.join(__dirname, 'public', 'index.html'),
+        }),
+    ],
+};
