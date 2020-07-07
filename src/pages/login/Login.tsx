0232e86fb302e9a99d70b7cfffa26a0cbda444f5
Andreasjj <andreasjj@gmail.com>
2020-07-07 14:35:10 +0200

    login page linter fixes

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { useKeycloak } from '@react-keycloak/web';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 const Wrapper = styled.div`
     display: flex;
