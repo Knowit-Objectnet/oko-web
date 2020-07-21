03a588e2881b00eac6349ce025ec53ae7387e2df
Andreasjj <andreasjj@gmail.com>
2020-07-21 14:13:36 +0200

    Updated GET and PATCH to use new url

--- src/pages/weightReporting/WeightReporting.tsx
+++ src/pages/weightReporting/WeightReporting.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { useKeycloak } from '@react-keycloak/web';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { PostToAPI } from '../../utils/PostToAPI';
 import { WithdrawalSubmission } from './WithdrawalSubmission';
-import { Colors, Withdrawal } from '../../types';
+import { apiUrl, Colors, Withdrawal } from '../../types';
 import useSWR from 'swr';
 import { fetcher } from '../../utils/fetcher';
 import { Loading } from '../loading/Loading';
+import {PatchToAPI} from "../../utils/PatchToAPI";
 
 const Wrapper = styled.div`
     display: flex;
@@ -52,57 +53,43 @@ export const WeightReporting: React.FC = () => {
     // Getting Keycloak instance
     const { keycloak } = useKeycloak();
 
-    const date = new Date('2020-07-17T14:12:40.665Z');
     // List of withdrawals fetched from the server
     const { data: apiWithdrawals, isValidating, mutate } = useSWR<Array<Withdrawal>>(
-        ['/api/withdrawals', keycloak.token],
+        [`${apiUrl}/weight-reporting/reports/?partner-id=${keycloak.tokenParsed.GroupID}`, keycloak.token],
         fetcher,
     );
-    const withdrawals =
-        apiWithdrawals && apiWithdrawals.length !== 0
-            ? apiWithdrawals.map((withdrawal: Withdrawal) => {
-                  withdrawal.start = new Date(withdrawal.start);
-                  withdrawal.end = new Date(withdrawal.end);
-                  return withdrawal;
-              })
-            : [
-                  {
-                      id: '1',
-                      start: date,
-                      end: date,
-                  },
-                  {
-                      id: '2',
-                      weight: 200,
-                      start: date,
-                      end: date,
-                  },
-                  {
-                      id: '3',
-                      weight: 200,
-                      start: date,
-                      end: date,
-                  },
-                  {
-                      id: '4',
-                      start: date,
-                      end: date,
-                  },
-              ];
+    // List of withdrawals transformed from the Api fetch
+    const [withdrawals, setWithdrawals] = useState<Array<Withdrawal> | null>(null);
+
+    useEffect(() => {
+        // If the api was successful and returned an array then transform it and update state
+        if (apiWithdrawals) {
+            // Transform the array from the api into a proper withdrawals list
+            const _withdrawals = apiWithdrawals.map((withdrawal: Withdrawal) => {
+                withdrawal.start = new Date(withdrawal.start);
+                withdrawal.end = new Date(withdrawal.end);
+                return withdrawal;
+            });
+            // Update the state
+            setWithdrawals(_withdrawals);
+        }
+    }, [apiWithdrawals]);
 
     const onSubmit = React.useCallback(async (weight: number, id: string) => {
         try {
-            // update the local data immediately, but disable the revalidation
-            const newWithdrawal = withdrawals.find((withdrawal) => withdrawal.id === id);
-            if (newWithdrawal) {
-                newWithdrawal.weight = weight;
-                mutate([...withdrawals, newWithdrawal], false);
+            if (withdrawals) {
+                // update the local data immediately, but disable the revalidation
+                const newWithdrawal = withdrawals.find((withdrawal) => withdrawal.id === id);
+                if (newWithdrawal) {
+                    newWithdrawal.weight = weight;
+                    mutate([...withdrawals, newWithdrawal], false);
 
-                // Post update to API
-                await PostToAPI('/api/weight', { weight }, keycloak.token);
+                    // Post update to API
+                    await PatchToAPI(`${apiUrl}/weight-reporting/reports/`, { id, weight }, keycloak.token);
 
-                // trigger a revalidation (refetch) to make sure our local data is correct
-                mutate();
+                    // trigger a revalidation (refetch) to make sure our local data is correct
+                    mutate();
+                }
             }
         } catch (err) {
             console.log(err);
@@ -111,30 +98,32 @@ export const WeightReporting: React.FC = () => {
 
     // Create a list of memoized elements such that we don't need to rerender every list element
     // when one gets updated
-    const withdrawalList = withdrawals.map((withdrawal) => (
-        <WithdrawalSubmission
-            key={withdrawal.id}
-            id={withdrawal.id}
-            weight={withdrawal.weight}
-            start={withdrawal.start}
-            end={withdrawal.end}
-            onSubmit={onSubmit}
-        />
-    ));
+    const withdrawalList =
+        withdrawals &&
+        withdrawals.map((withdrawal) => (
+            <WithdrawalSubmission
+                key={withdrawal.id}
+                id={withdrawal.id}
+                weight={withdrawal.weight}
+                start={withdrawal.start}
+                end={withdrawal.end}
+                onSubmit={onSubmit}
+            />
+        ));
 
     return (
         <Wrapper>
-            {(!withdrawals || withdrawals.length <= 0) && isValidating ? (
+            {!withdrawals && isValidating ? (
                 <Loading text="Laster inn data..." />
             ) : (
                 <Content>
                     <Latest>
                         <h2>Siste uttak</h2>
-                        {withdrawalList.slice(0, 1)}
+                        {withdrawalList && withdrawalList.slice(0, 1)}
                     </Latest>
                     <Older>
                         <h2>Tidligere uttak</h2>
-                        <OverflowWrapper>{withdrawalList.slice(1)}</OverflowWrapper>
+                        <OverflowWrapper>{withdrawalList && withdrawalList.slice(1)}</OverflowWrapper>
                     </Older>
                 </Content>
             )}
