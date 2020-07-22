16495f659ef417dbb843fcaa12fea18b27948701
Andreasjj <andreasjj@gmail.com>
2020-07-22 12:31:58 +0200

    Made title boldd

--- src/pages/MyPage/NewPartner.tsx
+++ src/pages/MyPage/NewPartner.tsx
@@ -17,6 +17,9 @@ const Title = styled.div`
     background-color: ${Colors.LightBeige};
     padding: 10px 20px;
     margin-bottom: 25px;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 22px;
 `;
 
 const Content = styled.div`
