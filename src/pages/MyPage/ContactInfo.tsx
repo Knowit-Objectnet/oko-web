657ddd7ac87d023eedd49c9561773366ac96f011
Andreasjj <andreasjj@gmail.com>
2020-07-27 16:03:20 +0200

    Added edit icon and submittion buttons

--- src/pages/MyPage/ContactInfo.tsx
+++ src/pages/MyPage/ContactInfo.tsx
@@ -5,6 +5,9 @@ import Person from '../../assets/Person.svg';
 import Phone from '../../assets/Phone.svg';
 import Mail from '../../assets/Mail.svg';
 import Pencil from '../../assets/Pencil.svg';
+import Cross from '../../assets/Cross.svg';
+import Check from '../../assets/Check.svg';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
     display: flex;
@@ -58,15 +61,31 @@ const StyledPencil = styled(Pencil)`
     height: 1em;
 `;
 
-const EditIcon = styled.div`
+const StyledCross = styled(Cross)`
+    height: 1em;
+`;
+
+const StyledCheck = styled(Check)`
+    height: 0.7em;
+`;
+
+interface IconProps {
+    color: Colors;
+}
+
+const Icon = styled.div<IconProps>`
     margin-left: 10px;
-    background-color: ${Colors.Blue};
+    background-color: ${(props) => props.color};
     border-radius: 50%;
     height: 27px;
     width: 27px;
     display: flex;
     align-items: center;
     justify-content: center;
+`;
+
+const EditOptions = styled.div`
+    display: flex;
 `;
 
 interface ContactInfoProps {
@@ -76,38 +95,57 @@ interface ContactInfoProps {
 /**
  * Contact info component
  */
-export const ContactInfo: React.FC<ContactInfoProps> = (props) => (
-    <Wrapper>
-        <TableTitle>
-            Kontaktinfo
-            <EditIcon>
-                <StyledPencil />
-            </EditIcon>
-        </TableTitle>
-        <Table>
-            <TableBody>
-                <tr>
-                    <td>
-                        <Cell>
-                            <StyledPerson height="1em" />
-                            <CellText>{props.info.name}</CellText>
-                        </Cell>
-                    </td>
-                    <td>
-                        <Cell>
-                            <StyledPhone height="1em" />
-                            <CellText>{props.info.phone || 'N/A'}</CellText>
-                        </Cell>
-                    </td>
-                    <td>
-                        <Cell>
-                            <StyledMail height="1em" />
-                            <CellText>{props.info.mail}</CellText>
-                        </Cell>
-                    </td>
-                    <td></td>
-                </tr>
-            </TableBody>
-        </Table>
-    </Wrapper>
-);
+export const ContactInfo: React.FC<ContactInfoProps> = (props) => {
+    const [editing, setEditing] = useState(false);
+
+    const onEditButtonClick = () => {
+        setEditing(!editing);
+    };
+
+    return (
+        <Wrapper>
+            <TableTitle>
+                Kontaktinfo
+                {editing ? (
+                    <EditOptions>
+                        <Icon color={Colors.Red} onClick={onEditButtonClick}>
+                            <StyledCross />
+                        </Icon>
+                        <Icon color={Colors.Green} onClick={onEditButtonClick}>
+                            <StyledCheck />
+                        </Icon>
+                    </EditOptions>
+                ) : (
+                    <Icon color={Colors.Blue} onClick={onEditButtonClick}>
+                        <StyledPencil />
+                    </Icon>
+                )}
+            </TableTitle>
+            <Table>
+                <TableBody>
+                    <tr>
+                        <td>
+                            <Cell>
+                                <StyledPerson height="1em" />
+                                <CellText>{props.info.name}</CellText>
+                            </Cell>
+                        </td>
+                        <td>
+                            <Cell>
+                                <StyledPhone height="1em" />
+                                <CellText>{props.info.phone || 'N/A'}</CellText>
+                            </Cell>
+                        </td>
+                        <td>
+                            <Cell>
+                                <StyledMail height="1em" />
+                                <CellText>{props.info.mail}</CellText>
+                            </Cell>
+                        </td>
+                        <td></td>
+                    </tr>
+                </TableBody>
+            </Table>
+        </Wrapper>
+    );
+};
