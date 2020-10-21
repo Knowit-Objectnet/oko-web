e7712cdb8c06b8a279ad7ded273b95636079a0ed
Thomas B. Aschim <thomas.aschim@knowit.no>
2020-10-21 14:25:18 +0200

    OKO-439 Added types to API methods for Event

--- src/httpclient/eventClients.ts
+++ src/httpclient/eventClients.ts
@@ -1,4 +1,4 @@
-import { ApiEvent, ApiEventPost } from '../types';
+import { ApiEvent, ApiEventPatch, ApiEventPost } from '../types';
 import { apiClient } from './apiClient';
 
 export interface ApiEventParams {
@@ -13,15 +13,20 @@ export interface ApiEventParams {
 // TODO, first parameter is the query key, could be stripped out somewhere else before calling this method
 export const getEvents = (_: string, params: ApiEventParams, token: string): Promise<Array<ApiEvent>> =>
     apiClient(token)
-        .get('/events', { params })
+        .get<Array<ApiEvent>>('/events', { params })
         .then((response) => response.data);
 
 export const postEvent = (newEvent: ApiEventPost, token: string): Promise<ApiEvent> =>
     apiClient(token)
-        .post('/events', newEvent)
+        .post<ApiEvent>('/events', newEvent)
         .then((response) => response.data);
 
 export const deleteEvents = (params: ApiEventParams, token: string): Promise<Array<ApiEvent>> =>
     apiClient(token)
-        .delete('/events', { params })
+        .delete<Array<ApiEvent>>('/events', { params })
+        .then((response) => response.data);
+
+export const patchEvent = (updatedEvent: ApiEventPatch, token: string): Promise<ApiEvent> =>
+    apiClient(token)
+        .patch<ApiEvent>('/events', updatedEvent)
         .then((response) => response.data);
