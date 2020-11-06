03480975936bec6f8a22d0cd59217e40340207c8
Thomas B. Aschim <thomas.aschim@knowit.no>
2020-11-06 14:17:05 +0100

    OKO-445 Created common form <select> component for Partner

---
+++ src/sharedComponents/forms/PartnerSelect.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { usePartners } from '../../api/hooks/usePartners';
+import styled from 'styled-components';
+
+const Select = styled.select`
+    width: 100%;
+    min-width: 250px;
+    height: 30px;
+    margin-bottom: 10px;
+`;
+
+interface Props {
+    selectedPartnerId?: number;
+    onSelectedPartnerChange: (partnerId: number) => void;
+}
+
+export const PartnerSelect: React.FC<Props> = (props) => {
+    const { data: partners, isLoading, isError } = usePartners();
+
+    const handleSelectedPartnerChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        props.onSelectedPartnerChange(parseInt(event.currentTarget.value));
+    };
+
+    return (
+        <Select
+            onChange={handleSelectedPartnerChange}
+            disabled={isLoading || isError}
+            value={props.selectedPartnerId ?? 'default'}
+        >
+            <option value="default" disabled>
+                {(isLoading && 'Laster inn...') ||
+                    (isError && 'Kunne ikke laste samarbeidspartnere') ||
+                    'Velg samarbeidspartner'}
+            </option>
+            {partners?.map((partner) => (
+                <option value={partner.id} key={partner.id}>
+                    {partner.name}
+                </option>
+            ))}
+        </Select>
+    );
+};
