03a86303d2acb80de974fa7251ca09d6d05d74e9
Thomas B. Aschim <thomas.aschim@knowit.no>
2020-11-06 15:41:37 +0100

    OKO-445 Removed prefetching of Partners

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,12 @@ import keycloak from './keycloak';
 import { RouterComponent } from './router/router';
 import { GlobalStyle } from './global-styles';
 import { SWRConfig } from 'swr';
-import { preFetch } from './pre-fetch';
 import ModalProvider from './sharedComponents/Modal/Provider';
 import AlertTemplate from 'react-alert-template-basic';
 import { positions, Provider as AlertProvider, transitions } from 'react-alert';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import { Helmet } from 'react-helmet';
-
-// Pre-fetch data with a token if the user is logged in
-const onKeycloakTokens = (tokens: { idToken: string; refreshToken: string; token: string }) => {
-    if (tokens.token) {
-        preFetch(tokens.token);
-    }
-};
 
 const alertOptions = {
     position: positions.TOP_CENTER,
@@ -28,7 +20,7 @@ const alertOptions = {
 
 export const App: React.FC = () => {
     return (
-        <KeycloakProvider keycloak={keycloak} onTokens={onKeycloakTokens}>
+        <KeycloakProvider keycloak={keycloak}>
             <ThemeProvider theme={theme}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
                     <ModalProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import { injectFont } from './font-injection';
-import { preFetch } from './pre-fetch';
 
-// Functions to call when the initial render is done
-const callback = () => {
+const runAfterFirstRender = () => {
     // Inject the Oslo font into the css
     injectFont();
-    // Pre-fetch data without a token
-    preFetch();
 };
 
-ReactDOM.render(<App />, document.getElementById('root'), callback);
+ReactDOM.render(<App />, document.getElementById('root'), runAfterFirstRender);
