b27dbc8266462d960b53e90b8d33f216d1760844
Thomas B. Aschim <thomas.aschim@knowit.no>
2020-11-25 15:57:12 +0100

    OKO-444 Moved Request approval spinner from button to parent component

--- src/pages/notifications/pickups/RequestApprovalButton.tsx
+++ src/pages/notifications/pickups/RequestApprovalButton.tsx
@@ -3,40 +3,33 @@ import { types, useAlert } from 'react-alert';
 import { useKeycloak } from '@react-keycloak/web';
 import { queryCache, useMutation } from 'react-query';
 import { ApiPickUpPatch, patchPickUp, pickUpsDefaultQueryKey } from '../../../api/PickUpService';
-import { useState } from 'react';
 import { PositiveButton } from '../../../sharedComponents/buttons/PositiveButton';
 
 interface Props {
     pickupId: number;
     partnerId: number;
-    requestApprovalLoading: boolean;
     onRequestApproval: (isLoading: boolean) => void;
 }
 
 export const RequestApprovalButton: React.FC<Props> = (props) => {
     const { keycloak } = useKeycloak();
     const alert = useAlert();
-    const [updatePickUpLoading, setUpdatePickUpLoading] = useState(false);
 
     const [updatePickUpMutation] = useMutation(
         (updatedPickUp: ApiPickUpPatch) => patchPickUp(updatedPickUp, keycloak.token),
         {
-            onMutate: () => {
-                setUpdatePickUpLoading(true);
-                props.onRequestApproval(true);
-            },
+            onSuccess: () => queryCache.invalidateQueries(pickUpsDefaultQueryKey),
             onError: () => {
-                setUpdatePickUpLoading(false);
-                props.onRequestApproval(false);
                 alert.show('Noe gikk galt, valg av samarbeidspartner til ekstrauttak ble ikke registrert.', {
                     type: types.ERROR,
                 });
             },
-            onSettled: () => queryCache.invalidateQueries(pickUpsDefaultQueryKey),
+            onSettled: () => props.onRequestApproval(false),
         },
     );
 
     const handleRequestApproval = () => {
+        props.onRequestApproval(true);
         const updatedPickUp: ApiPickUpPatch = {
             id: props.pickupId,
             chosenPartnerId: props.partnerId,
@@ -45,13 +38,7 @@ export const RequestApprovalButton: React.FC<Props> = (props) => {
     };
 
     return (
-        <PositiveButton
-            fillWidth
-            size="small"
-            onClick={handleRequestApproval}
-            isLoading={updatePickUpLoading}
-            disabled={props.requestApprovalLoading}
-        >
+        <PositiveButton fillWidth size="small" onClick={handleRequestApproval}>
             Godkjenn
         </PositiveButton>
     );
--- src/pages/notifications/pickups/RequestsStatusList.tsx
+++ src/pages/notifications/pickups/RequestsStatusList.tsx
@@ -7,6 +7,7 @@ import { useKeycloak } from '@react-keycloak/web';
 import { ApiRequest } from '../../../api/RequestService';
 import { useRequests } from '../../../api/hooks/useRequests';
 import { useState } from 'react';
+import DotsSpinner from '../../../assets/DotsSpinner.svg';
 
 const RequestList = styled.ul`
     display: flex;
@@ -20,13 +21,10 @@ const Request = styled.li`
     align-items: center;
     justify-content: space-between;
     flex: 1;
+    padding: 0.5rem 1rem;
 
     &:not(:last-child) {
         border-bottom: 0.125rem solid ${(props) => props.theme.colors.White};
-    }
-
-    & > * {
-        padding: 0.5rem 1rem;
     }
 `;
 
@@ -42,7 +40,11 @@ const Notice = styled.div`
 `;
 
 const StatusWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     min-width: 10rem;
+    min-height: 2.5rem;
 `;
 
 const Status = styled.div`
@@ -55,7 +57,6 @@ const Status = styled.div`
     background: ${(props) => props.theme.colors.White};
     border: 0.125rem solid;
     padding: 0.5rem 1rem;
-    min-height: 2.5rem;
     width: 100%;
 `;
 
@@ -67,6 +68,11 @@ const RejectedStatus = styled(Status)`
 `;
 const AwaitingStatus = styled(Status)`
     border-color: ${(props) => props.theme.colors.Blue};
+`;
+
+const LoadingSpinner = styled(DotsSpinner)`
+    height: 0.375rem;
+    width: auto;
 `;
 
 interface Props {
@@ -90,12 +96,13 @@ export const RequestsStatusList: React.FC<Props> = ({ pickUp }) => {
         const pickUpIsOpenForRequests = !pickUp.chosenPartner?.id;
         const thisRequestIsApproved = request.partner.id === pickUp.chosenPartner?.id;
 
-        if (pickUpIsOpenForRequests && userCanApproveRequests) {
+        if (requestApprovalLoading) {
+            return <LoadingSpinner />;
+        } else if (pickUpIsOpenForRequests && userCanApproveRequests) {
             return (
                 <RequestApprovalButton
                     pickupId={request.pickup.id}
                     partnerId={request.partner.id}
-                    requestApprovalLoading={requestApprovalLoading}
                     onRequestApproval={setRequestApprovalLoading}
                 />
             );
