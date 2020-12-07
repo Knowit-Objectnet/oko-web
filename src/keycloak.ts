6c08e83f435a706220b2cc313b17cd1e5efc8c6d
Thomas B. Aschim <thomas.aschim@knowit.no>
2020-12-07 16:28:21 +0100

    OKO-462 Added Keycloak realm environment variable

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,9 +1,8 @@
 import Keycloak from 'keycloak-js';
 
-// Setup Keycloak instance as needed
-// Pass initialization options as required or leave blank to load from 'keycloak.json'
+// Setup Keycloak instance
 const keycloak = new Keycloak({
-    realm: 'staging',
+    realm: process.env.KEYCLOAK_REALM,
     url: process.env.KEYCLOAK_URL,
     sslRequired: 'external',
     resource: 'react-app',
