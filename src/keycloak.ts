2c71b1298eac983a1775e306e0a7eb5a8b9db074
Thomas B. Aschim <thomas.aschim@knowit.no>
2020-12-09 12:04:17 +0100

    OKO-462 Changed environment variable names

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -2,8 +2,8 @@ import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance
 const keycloak = new Keycloak({
-    realm: process.env.KEYCLOAK_REALM,
-    url: process.env.KEYCLOAK_URL,
+    realm: process.env.REACT_APP_KEYCLOAK_REALM,
+    url: process.env.REACT_APP_KEYCLOAK_URL,
     sslRequired: 'external',
     resource: 'react-app',
     clientId: 'react-app',
