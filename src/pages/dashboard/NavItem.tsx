4fa9340e970584267ccf7178fdcb45f804edacb8
Eirik Osnes <eirik.osnes@gmail.com>
2021-04-23 16:49:03 +0200

    Refactored lambda functions

--- src/pages/dashboard/NavItem.tsx
+++ src/pages/dashboard/NavItem.tsx
@@ -3,39 +3,35 @@ import { NavLink, NavLinkProps } from 'react-router-dom';
 import { Box, Flex, Link, LinkProps } from '@chakra-ui/layout';
 import Icon from '@chakra-ui/icon';
 
-const StyledNavLink: React.FC<LinkProps | NavLinkProps> = (props) => {
-    return (
-        <Link
-            as={NavLink}
-            color="White"
-            borderBottom={{ base: 'none', sm: 'solid 4px transparent' }}
-            display="flex"
-            alignItems="flex-end"
-            userSelect="none"
-            fontWeight="bold"
-            fontSize={{ base: 'md', sm: 'xl' }}
-            _activeLink={{
-                color: 'secondary.light',
-                borderBottomColor: 'secondary.light',
-                svg: { fill: 'secondary.light' },
-            }}
-            {...props}
-        />
-    );
-};
+const StyledNavLink: React.FC<LinkProps | NavLinkProps> = (props) => (
+    <Link
+        as={NavLink}
+        color="White"
+        borderBottom={{ base: 'none', sm: 'solid 4px transparent' }}
+        display="flex"
+        alignItems="flex-end"
+        userSelect="none"
+        fontWeight="bold"
+        fontSize={{ base: 'md', sm: 'xl' }}
+        _activeLink={{
+            color: 'secondary.light',
+            borderBottomColor: 'secondary.light',
+            svg: { fill: 'secondary.light' },
+        }}
+        {...props}
+    />
+);
 
-const Center: React.FC = (props) => {
-    return (
-        <Flex
-            alignItems="center"
-            justifyContent="flex-end"
-            height={{ base: '100%', sm: 'auto' }}
-            flexDirection={{ base: 'column', lg: 'row' }}
-            _activeLink={{ color: 'red.500' }}
-            {...props}
-        />
-    );
-};
+const Center: React.FC = (props) => (
+    <Flex
+        alignItems="center"
+        justifyContent="flex-end"
+        height={{ base: '100%', sm: 'auto' }}
+        flexDirection={{ base: 'column', lg: 'row' }}
+        _activeLink={{ color: 'red.500' }}
+        {...props}
+    />
+);
 
 const iconStyle = {
     height: { base: 'auto', sm: '1em' },
