595fc15a44dbe2b557a07af3d0528bfc420b7121
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-04-27 15:51:32 +0200

    Gjort det mulig å sende med ReactQuery options-objekt med useEvents hook

--- src/services/hooks/useEvents.ts
+++ src/services/hooks/useEvents.ts
@@ -1,9 +1,13 @@
-import { QueryObserverResult, useQuery } from 'react-query';
+import { UseQueryResult, useQuery, UseQueryOptions } from 'react-query';
 import { ApiEvent, ApiEventParams, getEvents, eventsDefaultQueryKey } from '../EventService';
 
-export const useEvents = (params: ApiEventParams = {}): QueryObserverResult<Array<ApiEvent>> => {
+export const useEvents = (
+    params: ApiEventParams = {},
+    queryOptions?: UseQueryOptions<Array<ApiEvent>>,
+): UseQueryResult<Array<ApiEvent>> => {
     return useQuery<Array<ApiEvent>>({
         queryKey: [eventsDefaultQueryKey, params],
         queryFn: () => getEvents(params),
+        ...queryOptions,
     });
 };
