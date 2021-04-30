f95a7a1d9ee4efba994ba33234a6c5855b32f7a2
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-04-30 14:45:39 +0200

    Refaktorert filtrering av hentinger

--- src/pages/calendar/ReactBigCalendar.tsx
+++ src/pages/calendar/ReactBigCalendar.tsx
@@ -3,19 +3,30 @@ import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import { nb } from 'date-fns/locale';
 import { format, getDay, parse, startOfWeek } from 'date-fns';
 import { CalendarView, useCalendar } from './CalendarProvider';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useEvents } from '../../services/hooks/useEvents';
+
+// TODO: write our own CSS for the calendar
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 export const ReactBigCalendar: React.FC = () => {
     const { state, dispatch } = useCalendar();
 
+    // TODO: extract event-fetching/filtering-logic into hook
     // TODO: wrap in Lazy Result?
     // TODO: prefetch pre- and post-events based on view
     const { data: events } = useEvents({
         fromDate: state.viewDateRange.start.toISOString(),
         toDate: state.viewDateRange.end.toISOString(),
-        stationId: state.filters.stasjonId, // TODO: separate this out from query (removes unnecessary refetches)
     });
+    let filteredEvents = events ?? [];
+    if (state.filters.stasjonId !== undefined) {
+        filteredEvents = filteredEvents.filter((event) => event.station.id === state.filters.stasjonId);
+    }
+    const filteredAndTransformedEvents = filteredEvents.map((event) => ({
+        start: new Date(event.startDateTime),
+        end: new Date(event.endDateTime),
+        title: `${event.partner.name} - ${event.station.name}`,
+    }));
 
     const handleDateChange = (date: Date) => {
         dispatch({ type: 'SET_DATE', date });
@@ -37,11 +48,7 @@ export const ReactBigCalendar: React.FC = () => {
         <Calendar
             localizer={bigCalendarLocalizer}
             culture="nb-no"
-            events={(events ?? []).map((event) => ({
-                start: new Date(event.startDateTime),
-                end: new Date(event.endDateTime),
-                title: `${event.partner.name} - ${event.station.name}`,
-            }))}
+            events={filteredAndTransformedEvents}
             date={state.selectedDate}
             onNavigate={handleDateChange}
             onView={handleViewChange}
