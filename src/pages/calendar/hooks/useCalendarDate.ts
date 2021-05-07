9c9532ae02e403ddd274d7b4849f868f48b07593
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-05-07 16:28:23 +0200

    Validering av datoformat fra URL

--- src/pages/calendar/hooks/useCalendarDate.ts
+++ src/pages/calendar/hooks/useCalendarDate.ts
@@ -1,11 +1,15 @@
-import { formatISO, parseISO } from 'date-fns';
+import { formatISO, isValid, parseISO } from 'date-fns';
 import { useQueryStringKey } from 'use-route-as-state';
+
+const parseDate = (date?: string) => {
+    const parsedDate = parseISO(date ?? '');
+    return isValid(parsedDate) ? parsedDate : new Date();
+};
 
 export const useCalendarDate = (): [Date, (date: Date) => void] => {
     const [queryDate, setQueryDate] = useQueryStringKey('dato');
 
-    // TODO: handle invalid date format
-    const date: Date = queryDate ? parseISO(queryDate) : new Date();
+    const date = parseDate(queryDate);
 
     const setDate = (date: Date) => {
         const newDate = formatISO(date, { representation: 'date' });
