5433e2b600dead95e80cba39778a791273f8b090
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-05-10 11:23:49 +0200

    Test for å hente internt view-navn fra RBC view-navn

--- src/pages/calendar/hooks/useCalendarView.test.ts
+++ src/pages/calendar/hooks/useCalendarView.test.ts
@@ -1,4 +1,5 @@
-import { isValidView } from './useCalendarView';
+import { CalendarView, getCalendarViewFromType, isValidView } from './useCalendarView';
+import { View } from 'react-big-calendar';
 
 describe('Validation of view string', () => {
     const validViewStrings: string[] = ['uke', 'maned', 'dag', 'liste'];
@@ -23,5 +24,25 @@ describe('Validation of view string', () => {
     it.each(invalidViewStrings)('%p is validated as invalid calendar view', (viewString) => {
         const result = isValidView(viewString);
         expect(result).toBe(false);
+    });
+});
+
+describe('Get internal view type from React Big Calendar view type mapping', () => {
+    const validViewTypes: Array<[string, CalendarView]> = [
+        ['day', 'dag'],
+        ['work_week', 'uke'],
+        ['month', 'maned'],
+        ['agenda', 'liste'],
+    ];
+
+    it.each(validViewTypes)('%p React Big Calendar view type returns internal view type of %p', (viewType, view) => {
+        const result = getCalendarViewFromType(viewType as View);
+        expect(result).toBe(view);
+    });
+
+    const invalidViewTypes: (string | undefined)[] = ['week', 'list', undefined, 'undefined', 'invalid'];
+
+    it.each(invalidViewTypes)('%p invalid view type throws error', (viewType) => {
+        expect(() => getCalendarViewFromType(viewType as View)).toThrowError();
     });
 });
