767b411411fa18072ad68d07a62e6c72011902d8
Eirik Osnes <eirik.osnes@gmail.com>
2021-05-18 13:14:01 +0200

    Added mock data as per the current API

---
+++ __mocks__/mocks-new/mockAvtale.ts
@@ -0,0 +1,28 @@
+import { ApiAvtale } from '../../src/services-new/AvtaleService';
+
+export const mockAvtaler: Array<ApiAvtale> = [
+    {
+        id: '0067d96e-f7bf-4228-bca0-6f41462fe92c',
+        aktorId: 'ebde26e5-bb66-4c6c-8cd7-e11d6cbd00f2',
+        type: 'FAST',
+        startDato: '2021-01-01T',
+        sluttDato: '2022-01-01T',
+        henteplaner: [],
+    },
+    {
+        id: '1bb86d8c-f5cc-476e-852c-39c50fd29ce4',
+        aktorId: '20aafe35-eb86-4fe4-a9a8-6432e2109638',
+        type: 'FAST',
+        startDato: '2021-01-01T',
+        sluttDato: '2022-01-01T',
+        henteplaner: [],
+    },
+    {
+        id: 'f2b43dd4-7afd-4199-b92c-2cb236017ffa',
+        aktorId: 'b370a928-f5f9-48d6-b9f3-52533123fc4a',
+        type: 'ANNEN',
+        startDato: '2021-01-01T',
+        sluttDato: '2022-01-01T',
+        henteplaner: [],
+    },
+];
