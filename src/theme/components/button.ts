39e01c0fa78b413471c7a0c2f9634997c08f5a54
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-05-25 16:15:19 +0200

    Oppdatert outline knappe-stil

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -1,5 +1,34 @@
 export const Button = {
     baseStyle: {
         fontWeight: 'normal',
+        borderRadius: '0',
+    },
+    variants: {
+        outline: {
+            bg: 'surface',
+            color: 'onSurface',
+            borderColor: 'onSurface',
+            fill: 'onSurface',
+            // TODO: we should use [layer styles](https://chakra-ui.com/docs/features/text-and-layer-styles#layer-style)
+            //  here to avoid duplication - as soon as [this bug is fixed](https://github.com/chakra-ui/chakra-ui/issues/3883)
+            _focus: {
+                bg: 'onSurface',
+                color: 'surface',
+                fill: 'surface',
+                textDecoration: 'underline',
+            },
+            _active: {
+                bg: 'onSurface',
+                color: 'surface',
+                fill: 'surface',
+                textDecoration: 'underline',
+            },
+            _hover: {
+                bg: 'onSurface',
+                color: 'surface',
+                fill: 'surface',
+                textDecoration: 'underline',
+            },
+        },
     },
 };
