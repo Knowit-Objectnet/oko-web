54a383dc88dd69665328535a48f971e9e8284fdc
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-05-26 11:43:38 +0200

    Lagt til avkrysningsboks for om partner er ideell organisasjon

--- src/pages/avtaler/AddPartnerForm.tsx
+++ src/pages/avtaler/AddPartnerForm.tsx
@@ -4,13 +4,18 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { TextInput } from '../../components/forms/TextInput';
 import { PartnerStorrelse } from '../../types';
-import { Button } from '@chakra-ui/react';
+import { Button, Checkbox } from '@chakra-ui/react';
 import { Select, SelectOption } from '../../components/forms/Select';
+import { ApiPartnerPost } from '../../services-currentapi/AktorService';
 
 yup.setLocale({
     mixed: {
         required: '${label} er påkrevd',
         oneOf: 'Det må velges en ${label}',
+    },
+    string: {
+        min: '${label} må bestå av minst ${min} tegn',
+        // max: '${label} må være ikke være lenger enn ${max} tegn',
     },
 });
 
@@ -22,7 +27,7 @@ const storrelseOptions: Array<SelectOption<PartnerStorrelse>> = [
 
 const validationSchema = yup.object().shape({
     navn: yup.string().label('Navn for samarbeidspartneren').required().min(2),
-    // ideell: yup.boolean().label('Om partneren er en ideell organisasjon').required(),
+    ideell: yup.boolean().label('Om partneren er en ideell organisasjon').required(),
     storrelse: yup
         .mixed<PartnerStorrelse>()
         .label('størrelse på partneren')
@@ -35,7 +40,7 @@ interface Props {
 }
 
 export const AddPartnerForm: React.FC<Props> = ({ afterSubmit }) => {
-    const formMethods = useForm<{ navn: string; storrelse: PartnerStorrelse }>({
+    const formMethods = useForm<ApiPartnerPost>({
         resolver: yupResolver(validationSchema),
     });
 
@@ -49,6 +54,7 @@ export const AddPartnerForm: React.FC<Props> = ({ afterSubmit }) => {
             <form onSubmit={handleNewPartnerSubmission}>
                 <TextInput name="navn" label="Navn på organisasjonen" />
                 <Select name="storrelse" label="Størrelse" options={storrelseOptions} />
+                <Checkbox {...formMethods.register('ideell')}>Ideell organisasjon</Checkbox>
                 <Button type="submit" width="full">
                     Registrer ny partner
                 </Button>
