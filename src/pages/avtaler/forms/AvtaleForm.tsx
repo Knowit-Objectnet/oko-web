6e41c192600335147e8a9262ce2b2fa0e7a197c3
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-05-28 16:13:02 +0200

    Lagt til felt for avtaletype

--- src/pages/avtaler/forms/AvtaleForm.tsx
+++ src/pages/avtaler/forms/AvtaleForm.tsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import * as React from 'react';
+import * as yup from 'yup';
+import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { AvtaleType } from '../../../types';
+import { Stack } from '@chakra-ui/react';
+import { Select, SelectOption } from '../../../components/forms/Select';
+import { AllFormErrorMessages } from '../../../components/forms/AllFormErrorMessages';
 import { ApiPartner } from '../../../services-new/AktorService';
+import { RequiredFieldsInstruction } from '../../../components/forms/RequiredFieldsInstruction';
+import { upperFirst } from 'lodash';
+import { ApiAvtalePost } from '../../../services-new/AvtaleService';
+import { FormSubmitButton } from '../../../components/forms/FormSubmitButton';
+
+const avtaleTypeOptions: Array<SelectOption<AvtaleType>> = [
+    { value: 'FAST', label: 'Fast' },
+    { value: 'ANNEN', label: 'Annen' },
+];
+
+yup.setLocale({
+    string: {
+        min: ({ label, min }: { label: string; min: number }) => `${upperFirst(label)} må bestå av minst ${min} tegn`,
+        max: ({ label, max }: { label: string; max: number }) =>
+            `${upperFirst(label)} må være ikke være lenger enn ${max} tegn`,
+    },
+    mixed: {
+        required: 'Du må oppgi ${label}',
+        oneOf: 'Du må velge ${label}',
+    },
+});
+
+const validationSchema = yup.object().shape({
+    type: yup
+        .mixed()
+        .label('type for avtalen')
+        .required()
+        .oneOf(Object.values(avtaleTypeOptions).map((avtaleType) => avtaleType.value)),
+});
 
 interface Props {
     partner: ApiPartner;
@@ -7,5 +43,35 @@ interface Props {
 }
 
 export const AvtaleForm: React.FC<Props> = ({ partner, afterSubmit }) => {
-    return null;
+    const formMethods = useForm<ApiAvtalePost>({
+        resolver: yupResolver(validationSchema),
+        // TODO: if form is in edit mode: pass original values as "defaultValues" here
+    });
+
+    const handleAvtaleSubmission = formMethods.handleSubmit((data) => {
+        console.log({ ...data, aktorId: partner.id });
+        // TODO: submit data to API with useMutation (react-query) (post or patch, depending on form is in edit mode)
+        //  - pass loading state to button / disable form
+        //  - pass errors from backend response (onError react-query callback):
+        afterSubmit?.();
+    });
+
+    return (
+        <FormProvider {...formMethods}>
+            <form onSubmit={handleAvtaleSubmission}>
+                <Stack direction="column" spacing="8">
+                    <RequiredFieldsInstruction />
+                    <Select
+                        name="type"
+                        label="Type avtale"
+                        options={avtaleTypeOptions}
+                        placeholder="Velg en type"
+                        required
+                    />
+                    <AllFormErrorMessages />
+                    <FormSubmitButton label="Registrer ny avtale" />
+                </Stack>
+            </form>
+        </FormProvider>
+    );
 };
