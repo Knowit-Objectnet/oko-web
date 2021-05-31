7a661813cc99b752182d4f8af1d18bfc93a970c6
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-05-31 10:46:50 +0200

    Første versjon av nytt datovelger-felt

--- src/pages/avtaler/forms/AvtaleForm.tsx
+++ src/pages/avtaler/forms/AvtaleForm.tsx
@@ -9,8 +9,11 @@ import { AllFormErrorMessages } from '../../../components/forms/AllFormErrorMess
 import { ApiPartner } from '../../../services-new/AktorService';
 import { RequiredFieldsInstruction } from '../../../components/forms/RequiredFieldsInstruction';
 import { upperFirst } from 'lodash';
-import { ApiAvtalePost } from '../../../services-new/AvtaleService';
 import { FormSubmitButton } from '../../../components/forms/FormSubmitButton';
+import { DatePicker } from '../../../components/forms/DatePicker';
+import { formatISO, parse } from 'date-fns';
+import isDate from 'date-fns/isDate';
+import isValid from 'date-fns/isValid';
 
 const avtaleTypeOptions: Array<SelectOption<AvtaleType>> = [
     { value: 'FAST', label: 'Fast' },
@@ -35,6 +38,20 @@ const validationSchema = yup.object().shape({
         .label('type for avtalen')
         .required()
         .oneOf(Object.values(avtaleTypeOptions).map((avtaleType) => avtaleType.value)),
+    startDato: yup
+        .date()
+        .label('startdato for avtalen')
+        .transform((value, originalValue) => {
+            console.log(value);
+            if (isDate(value) && isValid(value)) {
+                return value;
+            }
+            const parsed = parse(originalValue, 'dd/MM/yyyy', new Date());
+            console.log(parsed);
+            return isValid(parsed) ? parsed : null;
+        })
+        .required()
+        .nullable(),
 });
 
 interface Props {
@@ -42,14 +59,21 @@ interface Props {
     afterSubmit?: () => void;
 }
 
+interface AvtaleFormData {
+    type: AvtaleType;
+    startDato: Date;
+    // sluttDato: Date; //LocalDate
+}
+
 export const AvtaleForm: React.FC<Props> = ({ partner, afterSubmit }) => {
-    const formMethods = useForm<ApiAvtalePost>({
+    const formMethods = useForm<AvtaleFormData>({
         resolver: yupResolver(validationSchema),
         // TODO: if form is in edit mode: pass original values as "defaultValues" here
     });
 
     const handleAvtaleSubmission = formMethods.handleSubmit((data) => {
-        console.log({ ...data, aktorId: partner.id });
+        console.log(data.startDato);
+        console.log({ startDato: formatISO(data.startDato), type: data.type, aktorId: partner.id });
         // TODO: submit data to API with useMutation (react-query) (post or patch, depending on form is in edit mode)
         //  - pass loading state to button / disable form
         //  - pass errors from backend response (onError react-query callback):
@@ -68,6 +92,8 @@ export const AvtaleForm: React.FC<Props> = ({ partner, afterSubmit }) => {
                         placeholder="Velg en type"
                         required
                     />
+                    <DatePicker name="startDato" label="Startdato for avtalen" required />
+                    {/*TODO: form errors displayed even if no errors */}
                     <AllFormErrorMessages />
                     <FormSubmitButton label="Registrer ny avtale" />
                 </Stack>
