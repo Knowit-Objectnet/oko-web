2e37ad3c3f2ed0ae907426bc7f88ff72c22e014f
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-03 17:33:09 +0200

    Laget støtte for invalidering av flere cache-nøkler ved mutasjon

--- src/services/henteplan/useAddHenteplan.ts
+++ src/services/henteplan/useAddHenteplan.ts
@@ -2,9 +2,11 @@ import { UseMutationResult } from 'react-query';
 import { useMutationWithInvalidation } from '../useMutationWithInvalidation';
 import { ApiError } from '../httpClient';
 import { ApiHenteplan, ApiHenteplanPost, henteplanDefaultQueryKey, postHenteplan } from './HenteplanService';
+import { avtaleDefaultQueryKey } from '../avtale/AvtaleService';
+import { planlagtHentingDefaultQueryKey } from '../henting/HentingService';
 
 export const useAddHenteplan = (): UseMutationResult<ApiHenteplan, ApiError, ApiHenteplanPost> =>
     useMutationWithInvalidation<ApiHenteplan, ApiError, ApiHenteplanPost>(
         (newHenteplan) => postHenteplan(newHenteplan),
-        henteplanDefaultQueryKey,
+        [henteplanDefaultQueryKey, avtaleDefaultQueryKey, planlagtHentingDefaultQueryKey],
     );
--- src/services/stasjon/useAddStasjon.ts
+++ src/services/stasjon/useAddStasjon.ts
@@ -4,7 +4,6 @@ import { useMutationWithInvalidation } from '../useMutationWithInvalidation';
 import { ApiError } from '../httpClient';
 
 export const useAddStasjon = (): UseMutationResult<ApiStasjon, ApiError, ApiStasjonPost> =>
-    useMutationWithInvalidation<ApiStasjon, ApiError, ApiStasjonPost>(
-        (newStasjon) => postStasjon(newStasjon),
+    useMutationWithInvalidation<ApiStasjon, ApiError, ApiStasjonPost>((newStasjon) => postStasjon(newStasjon), [
         stasjonDefaultQueryKey,
-    );
+    ]);
