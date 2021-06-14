71faec5594a7ef448de38d478a0f75118f51a4e8
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-14 10:33:50 +0200

    Lagt til typer for KontaktService

--- src/services/aktor/KontaktService.ts
+++ src/services/aktor/KontaktService.ts
@@ -1,16 +1,42 @@
 export interface ApiKontakt {
     id: string;
+    aktorId: string;
     navn: string;
-    telefon: string;
-    epost: string;
-    rolle: string;
+    telefon?: string;
+    epost?: string;
+    rolle?: string;
 }
 
 export interface ApiKontaktPost {
     navn: string;
-    telefon: string;
-    epost: string;
-    rolle: string;
+    telefon?: string;
+    epost?: string;
+    rolle?: string;
+}
+
+export interface ApiKontaktPatch {
+    id: string;
+    navn?: string;
+    telefon?: string;
+    epost?: string;
+    rolle?: string;
+}
+
+export interface ApiKontaktPatch {
+    id: string;
+    navn?: string;
+    telefon?: string;
+    epost?: string;
+    rolle?: string;
+}
+
+export interface ApiKontaktParams {
+    id?: string;
+    aktorId?: string;
+    navn?: string;
+    telefon?: string;
+    epost?: string;
+    rolle?: string;
 }
 
 const kontaktEndpoint = '/kontakter';
