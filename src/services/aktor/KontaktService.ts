36256b878be154453d4db7090f7c680eb31537bd
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-14 10:35:53 +0200

    Lagt til REST-metoder for KontaktService

--- src/services/aktor/KontaktService.ts
+++ src/services/aktor/KontaktService.ts
@@ -1,3 +1,5 @@
+import { extractResponse, httpClient, transformError } from '../httpClient';
+
 export interface ApiKontakt {
     id: string;
     aktorId: string;
@@ -41,3 +43,15 @@ export interface ApiKontaktParams {
 
 const kontaktEndpoint = '/kontakter';
 export const kontaktDefaultQueryKey = 'getKontakt';
+
+export const getKontaktr = (params: ApiKontaktParams = {}): Promise<Array<ApiKontakt>> =>
+    httpClient().get<Array<ApiKontakt>>(kontaktEndpoint, { params }).then(extractResponse, transformError);
+
+export const getKontaktById = (kontaktId: string): Promise<ApiKontakt> =>
+    httpClient().get<ApiKontakt>(`${kontaktEndpoint}/${kontaktId}`).then(extractResponse, transformError);
+
+export const postKontakt = (newKontakt: ApiKontaktPost): Promise<ApiKontakt> =>
+    httpClient().post<ApiKontakt>(kontaktEndpoint, newKontakt).then(extractResponse, transformError);
+
+export const deleteKontakt = (kontaktId: string): Promise<ApiKontakt> =>
+    httpClient().delete<ApiKontakt>(`${kontaktEndpoint}/${kontaktId}`).then(extractResponse, transformError);
