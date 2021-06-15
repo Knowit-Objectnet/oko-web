cb1f436b3ff96f8aa6031010ce1e046040d2b640
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-15 10:02:44 +0200

    Første fungerende versjon av skjema for kontaktperson

--- src/pages/avtaler/kontaktperson/KontaktpersonForm.tsx
+++ src/pages/avtaler/kontaktperson/KontaktpersonForm.tsx
@@ -10,15 +10,25 @@ import { ErrorMessages } from '../../../components/forms/ErrorMessages';
 import { Input } from '../../../components/forms/Input';
 import { FormSubmitButton } from '../../../components/forms/FormSubmitButton';
 import { useState } from 'react';
+import { useAddKontakt } from '../../../services/aktor/useAddKontakt';
+import { upperFirst } from 'lodash';
+import { useSuccessToast } from '../../../components/toasts/useSuccessToast';
 
 // NB! Setting the error messages used by yup
 import '../../../utils/forms/formErrorMessages';
 
 const validationSchema = yup.object().shape({
-    navn: yup.string().label('navn på kontaktpersonen').trim().required().min(5),
-    // TODO: telefon: yup.string().label(' ').trim().required().regexpattern,
-    // TODO: epost: yup.string().label(' ').trim().required().regexpattern,
-    // TODO: rolle: yup.string().label(' ').trim().required().min(5),
+    navn: yup.string().label('navn på kontaktpersonen').trim().required().min(5), // TODO: regex for og etternavn
+    rolle: yup.string().label('kontaktpersonens rolle').trim().required().min(5),
+    telefon: yup
+        .string()
+        .label('telefonnummer for kontaktpersonen')
+        .trim()
+        .matches(/^(\+?(00)?(47))?[2-9]\d{7}$/, {
+            message: ({ label }: { label: string }) => `${upperFirst(label)} må være et gyldig, norsk telefonnummer`,
+            excludeEmptyString: true,
+        }),
+    epost: yup.string().label('e-postadresse for kontaktpersonen').trim().email(),
 });
 
 interface Props {
@@ -33,24 +43,32 @@ export const KontaktpersonForm: React.FC<Props> = ({ partner, onSuccess }) => {
         // TODO: if form is in edit mode: pass original values as "defaultValues" here
     });
 
+    const addKontaktMutation = useAddKontakt();
+    const showSuccessToast = useSuccessToast();
     const [apiOrNetworkError, setApiOrNetworkError] = useState<string>();
 
     const handleSubmit = formMethods.handleSubmit((data) => {
-        // setApiOrNetworkError(undefined);
+        setApiOrNetworkError(undefined);
+
+        const newKontakt: ApiKontaktPost = {
+            ...data,
+            aktorId: partner.id,
+        };
 
         console.log(data);
+        console.log(newKontakt);
 
-        // addKontaktMutation.mutate(data, {
-        //     onSuccess: () => {
-        //         showSuccessToast({ title:  });
-        //         onSuccess?.();
-        //     },
-        //     onError: (error) => {
-        //         // TODO: get details from error and set appropriate message.
-        //         //  If caused by user: set message to correct field
-        //         setApiOrNetworkError('Uffda, noe gikk galt ved registreringen. Vennligst prøv igjen.');
-        //     },
-        // });
+        addKontaktMutation.mutate(newKontakt, {
+            onSuccess: () => {
+                showSuccessToast({ title: `${data.navn} ble registrert som kontaktperson for ${partner.navn}` });
+                onSuccess?.();
+            },
+            onError: (error) => {
+                // TODO: get details from error and set appropriate message.
+                //  If caused by user: set message to correct field
+                setApiOrNetworkError('Uffda, noe gikk galt ved registreringen. Vennligst prøv igjen.');
+            },
+        });
     });
 
     return (
@@ -60,12 +78,17 @@ export const KontaktpersonForm: React.FC<Props> = ({ partner, onSuccess }) => {
                     <RequiredFieldsInstruction />
                     <ErrorMessages globalError={apiOrNetworkError} />
                     <Input name="navn" label="Navn" helperText="Skriv inn fullt navn (for- og etternavn)" required />
-                    <Input type="phone" name="telefon" label="Telefonnummer" helperText="TODO: FORMAT" required />
-                    <Input type="email" name="epost" label="E-postadresse" required />
-                    <Input name="rolle" label="TODO: select? Rolle" required />
+                    <Input name="rolle" label="Rolle" required />
+                    <Input
+                        type="phone"
+                        name="telefon"
+                        label="Telefonnummer"
+                        helperText="Må være et norsk telefonnummer"
+                    />
+                    <Input type="email" name="epost" label="E-postadresse" required={false} />
                     <FormSubmitButton
                         label="Registrer ny kontaktperson"
-                        // TODO : isLoading={addKontaktMutation.isLoading}
+                        isLoading={addKontaktMutation.isLoading}
                         loadingText="Vennligst vent..."
                     />
                 </Stack>
