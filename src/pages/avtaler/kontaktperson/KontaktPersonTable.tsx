384dc1a5c252ecad2d0ef70d609b93a93caa31c8
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-15 10:38:04 +0200

    Viser e-postadresse for kontaktperson

--- src/pages/avtaler/kontaktperson/KontaktPersonTable.tsx
+++ src/pages/avtaler/kontaktperson/KontaktPersonTable.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ButtonGroup, Table, Tbody, Td, Th, Thead, Tr, VisuallyHidden } from '@chakra-ui/react';
+import { ButtonGroup, Link, Table, Tbody, Td, Th, Thead, Tr, VisuallyHidden } from '@chakra-ui/react';
 import { EditButton } from '../../../components/buttons/EditButton';
 import { DeleteButton } from '../../../components/buttons/DeleteButton';
 import { ApiKontakt } from '../../../services/aktor/KontaktService';
@@ -20,29 +20,31 @@ export const KontaktPersonTable: React.FC<Props> = ({ kontaktPersoner }) => {
                     <Th scope="col">Navn</Th>
                     <Th scope="col">Rolle</Th>
                     <Th scope="col">Telefonnummer</Th>
+                    <Th scope="col">E-postadresse</Th>
                     <Th scope="col">
                         <VisuallyHidden>Handlinger</VisuallyHidden>
                     </Th>
                 </Tr>
             </Thead>
             <Tbody>
-                {sortedKontaktPersoner.map((person) => (
-                    <Tr key={person.id}>
-                        <Td>{person.navn}</Td>
-                        <Td>{person.rolle}</Td>
-                        <Td>{person.telefon}</Td>
+                {sortedKontaktPersoner.map(({ id, navn, rolle, telefon, epost }) => (
+                    <Tr key={id}>
+                        <Td>{navn}</Td>
+                        <Td>{rolle}</Td>
+                        <Td>{telefon}</Td>
+                        <Td>{epost ? <Link href={`mailto:${epost}`}>{epost}</Link> : null}</Td>
                         <Td textAlign="end">
                             <ButtonGroup spacing="4" size="sm">
                                 <EditButton
                                     label="Rediger"
                                     onClick={() => {
-                                        console.log(`Rediger ${person.navn}`);
+                                        console.log(`Rediger ${navn}`);
                                     }}
                                 />
                                 <DeleteButton
                                     label="Slett"
                                     onClick={() => {
-                                        console.log(`Slett ${person.navn}`);
+                                        console.log(`Slett ${navn}`);
                                     }}
                                 />
                             </ButtonGroup>
