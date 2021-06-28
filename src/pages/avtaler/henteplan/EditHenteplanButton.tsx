3675575c469fe7c2d2389e88b4859839a33bbc98
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-28 10:21:22 +0200

    Fiks etter rebase

--- src/pages/avtaler/henteplan/EditHenteplanButton.tsx
+++ src/pages/avtaler/henteplan/EditHenteplanButton.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ButtonProps, useDisclosure } from '@chakra-ui/react';
-import { FormModal } from '../../../components/forms/FormModal';
+import { Modal } from '../../../components/Modal';
 import { EditButton } from '../../../components/buttons/EditButton';
 import { ApiHenteplan } from '../../../services/henteplan/HenteplanService';
 import { ApiAvtale } from '../../../services/avtale/AvtaleService';
@@ -21,9 +21,9 @@ export const EditHenteplanButton: React.FC<Props & Omit<ButtonProps, 'onClick'>>
     return (
         <>
             <EditButton label="Rediger" aria-label="Rediger henteplanen" {...props} onClick={onOpen} />
-            <FormModal title="Rediger henteplan" isOpen={isOpen} onClose={onClose}>
+            <Modal title="Rediger henteplan" isOpen={isOpen} onClose={onClose}>
                 <EditHenteplanForm onSuccess={onClose} henteplan={henteplan} avtale={avtale} />
-            </FormModal>
+            </Modal>
         </>
     );
 };
