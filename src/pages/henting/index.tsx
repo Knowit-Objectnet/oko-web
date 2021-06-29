0057417f129a97fa1513d55e7b27181649cb9e76
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-29 13:35:51 +0200

    Forbedret versjon av detaljvisning

--- src/pages/henting/index.tsx
+++ src/pages/henting/index.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { Flex } from '@chakra-ui/layout';
+import { Box, Flex } from '@chakra-ui/layout';
 import { ApiPlanlagtHenting } from '../../services/henting/HentingService';
-import { useLocation, useRouteMatch } from 'react-router-dom';
+import { Link, useLocation, useRouteMatch } from 'react-router-dom';
 import { usePlanlagtHentingById } from '../../services/henting/usePlanlagtHentingById';
 import { formatDate, formatTime } from '../../utils/formatDateTime';
 import { parseISOIgnoreTimezone } from '../../utils/hentingDateTimeHelpers';
 import { KategoriList } from '../../components/KategoriList';
-import { VisuallyHidden } from '@chakra-ui/react';
+import { HStack, Icon, VStack, Badge, VisuallyHidden } from '@chakra-ui/react';
 import { isToday } from 'date-fns';
+import Calendar from '../../assets/Calendar.svg';
+import Clock from '../../assets/Clock.svg';
+import Location from '../../assets/Location.svg';
+import Warning from '../../assets/Warning.svg';
+import { CancelPlanlagtHentingButton } from './CancelPlanlagtHentingButton';
 
 const getDayString = (date: Date) => {
     if (isToday(date)) {
@@ -17,8 +22,23 @@ const getDayString = (date: Date) => {
     return formatDate(date);
 };
 
+export const AvlystBadge: React.FC = () => (
+    <Badge
+        as={Flex}
+        variant="solid"
+        backgroundColor="warning"
+        color="onWarning"
+        fontSize="0.8rem"
+        paddingY="0.5"
+        paddingX="1.5"
+        borderRadius={0}
+    >
+        <Icon as={Warning} transform="translateY(-2px)" /> Avlyst
+    </Badge>
+);
+
 const Henting: React.FC = () => {
-    const location = useLocation<{ henting?: ApiPlanlagtHenting }>();
+    const location = useLocation<{ henting?: ApiPlanlagtHenting; prevPath?: string }>();
 
     const routeMatch = useRouteMatch<{ hentingId?: string }>();
     const { data, isLoading, error } = usePlanlagtHentingById(routeMatch.params.hentingId || '', {
@@ -34,28 +54,70 @@ const Henting: React.FC = () => {
             </Helmet>
             {data ? (
                 <Flex as="main" width="full" height="full" backgroundColor="surface">
-                    <Flex width="4xl" maxWidth="4xl" marginX="auto" direction="column">
-                        <dl>
-                            <VisuallyHidden as="dt">Tidspunkt</VisuallyHidden>
-                            <dd>
-                                {getDayString(parseISOIgnoreTimezone(data?.startTidspunkt))}
-                                {` kl. `}
-                                {formatTime(parseISOIgnoreTimezone(data?.startTidspunkt))}
-                            </dd>
-                            <VisuallyHidden as="dt">Stasjon</VisuallyHidden>
-                            <dd>{data?.stasjonNavn}</dd>
+                    <Flex width="4xl" maxWidth="4xl" marginX="auto" direction="column" padding="10">
+                        {location.state?.prevPath ? <Link to={location.state.prevPath}>&laquo; Tilbake</Link> : null}
+                        <VStack as="dl" spacing="3" alignItems="flex-start" marginY="4">
+                            <Box>
+                                <VisuallyHidden>
+                                    <dt>Partner</dt>
+                                </VisuallyHidden>
+                                <dd>{data.aktorNavn}</dd>
+                            </Box>
+
+                            {data.avlyst ? (
+                                <Box>
+                                    <VisuallyHidden>
+                                        <dt>Status</dt>
+                                    </VisuallyHidden>
+                                    <dd>
+                                        <AvlystBadge />
+                                    </dd>
+                                </Box>
+                            ) : null}
+
+                            <HStack>
+                                <dt>
+                                    <Icon as={Location} aria-label="Stasjon" />
+                                </dt>
+                                <dd>{data.stasjonNavn}</dd>
+                            </HStack>
+                            <HStack>
+                                <dt>
+                                    <Icon as={Calendar} aria-label="Dato" />
+                                </dt>
+                                <dd>{getDayString(parseISOIgnoreTimezone(data?.startTidspunkt))}</dd>
+                            </HStack>
+                            <HStack>
+                                <dt>
+                                    <Icon as={Clock} aria-label="Tidspunkt" />
+                                </dt>
+                                <dd>
+                                    {`Fra kl. `}
+                                    {formatTime(parseISOIgnoreTimezone(data?.startTidspunkt))}
+                                    {` til kl. `}
+                                    {formatTime(parseISOIgnoreTimezone(data?.sluttTidspunkt))}
+                                </dd>
+                            </HStack>
+
                             {data?.merknad ? (
-                                <>
+                                <VStack spacing="1">
                                     <dt>Merknad</dt>
                                     <dd>{data.merknad}</dd>
-                                </>
+                                </VStack>
                             ) : null}
-                            <dt>Kategorier</dt>
-                            <dd>
-                                <KategoriList kategorier={(data?.kategorier || []).map(({ kategori }) => kategori)} />
-                            </dd>
-                        </dl>
-                        <button onClick={() => console.log('Avlys henting')}>Avlys</button>
+
+                            {data.kategorier.length > 0 ? (
+                                <VStack spacing="1" alignItems="flex-start">
+                                    <dt>Kategorier</dt>
+                                    <dd>
+                                        <KategoriList
+                                            kategorier={(data?.kategorier || []).map(({ kategori }) => kategori)}
+                                        />
+                                    </dd>
+                                </VStack>
+                            ) : null}
+                        </VStack>
+                        {!data.avlyst ? <CancelPlanlagtHentingButton henting={data} /> : null}
                     </Flex>
                 </Flex>
             ) : (
--- src/pages/kalender/components/EventComponent.tsx
+++ src/pages/kalender/components/EventComponent.tsx
@@ -1,42 +1,53 @@
 import React from 'react';
-import { LinkBox, LinkBoxProps, LinkOverlay } from '@chakra-ui/react';
+import { Icon, LinkBox, LinkBoxProps, LinkOverlay, Text } from '@chakra-ui/react';
 import { CalendarEvent } from '../hooks/useCalendarEvents';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { Box } from '@chakra-ui/layout';
+import Warning from '../../../assets/Warning.svg';
+import { AvlystBadge } from '../../henting';
 
 type Props = Pick<LinkBoxProps, 'position' | 'top' | 'left' | 'height' | 'width' | 'margin'> & {
     event: CalendarEvent;
 };
 
-export const EventComponent: React.FC<Props> = ({ event, ...props }) => (
-    <LinkBox
-        as="article"
-        display="block"
-        backgroundColor="surface"
-        color="onSurface"
-        borderRadius="4px"
-        border="1px solid"
-        borderColor="DarkBeige"
-        paddingY="2px"
-        paddingX="5px"
-        height="auto"
-        maxHeight="full"
-        width="full"
-        maxWidth="full"
-        overflow="hidden"
-        textOverflow="ellipsis"
-        fontSize="sm"
-        fontWeight="medium"
-        {...props}
-    >
-        <LinkOverlay
-            as={Link}
-            to={{
-                pathname: `/henting/${event.henting.id}`,
-                // We pass the henting object, to avoid unnecessary loading state in details view
-                state: { henting: event.henting },
-            }}
+export const EventComponent: React.FC<Props> = ({ event, ...props }) => {
+    const location = useLocation();
+
+    return (
+        <LinkBox
+            as="article"
+            display="block"
+            backgroundColor="surface"
+            color="onSurface"
+            borderRadius="4px"
+            border="1px solid"
+            borderColor="DarkBeige"
+            paddingY="2px"
+            paddingX="5px"
+            height="auto"
+            maxHeight="full"
+            width="full"
+            maxWidth="full"
+            overflow="hidden"
+            textOverflow="ellipsis"
+            fontSize="sm"
+            fontWeight="medium"
+            {...props}
         >
-            {event.title}
-        </LinkOverlay>
-    </LinkBox>
-);
+            <LinkOverlay
+                as={Link}
+                to={{
+                    pathname: `/henting/${event.henting.id}`,
+                    // We pass the henting object, to avoid unnecessary loading state in details view
+                    state: { henting: event.henting, prevPath: location.pathname + location.search },
+                }}
+            >
+                {/*<Box>*/}
+                {/*    {event.start}–{event.end}*/}
+                {/*</Box>*/}
+                <Box>{event.title}</Box>
+                {event.henting.avlyst ? <AvlystBadge /> : null}
+            </LinkOverlay>
+        </LinkBox>
+    );
+};
