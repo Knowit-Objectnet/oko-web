581b31966d3b1535ba9b5ee40f413c4d51f6098f
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-06-29 16:49:05 +0200

    Introdusert LazyResult-wrapper til bruk med useQuery

---
+++ src/services/useQueryWithLazyResult.ts
@@ -0,0 +1,22 @@
+import { useQuery, UseQueryOptions } from 'react-query';
+import { Failure, Initial, LazyResult, Loading, Success } from 'lemons';
+
+export const useQueryWithLazyResult = <TResult = unknown, TError = unknown>(
+    queryOptions: UseQueryOptions<TResult, TError>,
+): LazyResult<TError, TResult> => {
+    const { data, isLoading, error } = useQuery<TResult, TError>(queryOptions);
+
+    if (data) {
+        return Success(data);
+    }
+
+    if (isLoading) {
+        return Loading();
+    }
+
+    if (error) {
+        return Failure(error);
+    }
+
+    return Initial();
+};
