4455744d306457cf0156f634423baee72790bf57
naonto <nora.antonsen@hotmail.com>
2021-06-30 12:25:00 +0200

    Listevisning av kontaktpers. bokser og ikon

--- src/pages/avtaler/kontaktperson/KontaktPersonTable.tsx
+++ src/pages/avtaler/kontaktperson/KontaktPersonTable.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { ButtonGroup, Link, Table, Tbody, Td, Th, Thead, Tr, VisuallyHidden } from '@chakra-ui/react';
+import { ButtonGroup, Flex, Icon, Link } from '@chakra-ui/react';
 import { ApiKontakt } from '../../../services/aktor/KontaktService';
 import { EditKontaktPersonButton } from './EditKontaktPersonButton';
 import { DeleteKontaktPersonButton } from './DeleteKontaktPersonButton';
+import { List, ListItem } from '@chakra-ui/react';
+import DefaultProfilePic from '../../../assets/Default_profile_pic.svg';
 
 interface Props {
     kontaktPersoner: Array<ApiKontakt>;
@@ -14,34 +16,38 @@ export const KontaktPersonTable: React.FC<Props> = ({ kontaktPersoner }) => {
     );
 
     return (
-        <Table>
-            <Thead>
-                <Tr>
-                    <Th scope="col">Navn</Th>
-                    <Th scope="col">Rolle</Th>
-                    <Th scope="col">Mobiltelefon</Th>
-                    <Th scope="col">E-postadresse</Th>
-                    <Th scope="col">
-                        <VisuallyHidden>Handlinger</VisuallyHidden>
-                    </Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {sortedKontaktPersoner.map((kontakt) => (
-                    <Tr key={kontakt.id}>
-                        <Td>{kontakt.navn}</Td>
-                        <Td>{kontakt.rolle}</Td>
-                        <Td>{kontakt.telefon}</Td>
-                        <Td>{kontakt.epost ? <Link href={`mailto:${kontakt.epost}`}>{kontakt.epost}</Link> : null}</Td>
-                        <Td textAlign="end">
+        <List>
+            {sortedKontaktPersoner.map((kontakt) => (
+                <ListItem key={kontakt.id}>
+                    <Flex width="full" border="4px solid" borderColor="gray.200" margin="8px" key={kontakt.id}>
+                        <Flex padding="8px">
+                            <Icon as={DefaultProfilePic} height="30px" width="auto" margin="6" />
+                        </Flex>
+                        <Flex padding="8px" flexGrow={1}>
+                            <List fontSize="12px">
+                                <ListItem fontWeight="medium" fontSize="16px">
+                                    {kontakt.navn}
+                                </ListItem>
+                                <ListItem>{kontakt.rolle}</ListItem>
+                                <ListItem>{kontakt.telefon ? kontakt.telefon : <span>&nbsp;&nbsp;</span>}</ListItem>
+                                <ListItem>
+                                    {kontakt.epost ? (
+                                        <Link href={`mailto:${kontakt.epost}`}>{kontakt.epost}</Link>
+                                    ) : (
+                                        <span>&nbsp;&nbsp;</span>
+                                    )}
+                                </ListItem>
+                            </List>
+                        </Flex>
+                        <Flex padding="8px" textAlign="end">
                             <ButtonGroup spacing="4" size="sm">
                                 <EditKontaktPersonButton kontakt={kontakt} />
                                 <DeleteKontaktPersonButton kontakt={kontakt} />
                             </ButtonGroup>
-                        </Td>
-                    </Tr>
-                ))}
-            </Tbody>
-        </Table>
+                        </Flex>
+                    </Flex>
+                </ListItem>
+            ))}
+        </List>
     );
 };
