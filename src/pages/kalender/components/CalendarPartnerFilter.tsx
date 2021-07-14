ebbb49de7964c8e7841a694dceeec97463f5bd13
Eirik Osnes <eirik.osnes@gmail.com>
2021-07-14 10:25:58 +0200

    Fjernet ubrukte imports

--- src/pages/kalender/components/CalendarPartnerFilter.tsx
+++ src/pages/kalender/components/CalendarPartnerFilter.tsx
@@ -1,17 +1,6 @@
 import * as React from 'react';
-import { useState } from 'react';
-import Filter from '../../../assets/Filter.svg';
-import ArrowRight from '../../../assets/ArrowRight.svg';
-import ArrowDown from '../../../assets/ArrowDown.svg';
-import { useCalendarState } from '../CalendarProvider';
-import { useStasjoner } from '../../../services/stasjon/useStasjoner';
-import { ApiPlanlagtHenting } from '../../../services/henting/PlanlagtHentingService';
-import { Checkbox } from '../../../components/forms/checkbox/Checkbox';
-import { ApiHentingWrapper } from '../../../services/henting/HentingService';
 import { usePartnere } from '../../../services/partner/usePartnere';
-import { Flex, Icon } from '@chakra-ui/react';
 import { CalendarFilterSelect } from './CalendarFilterSelect';
-import Henting from '../../henting';
 
 export const CalendarPartnerFilter: React.FC = () => {
     const { data: partnere } = usePartnere();
