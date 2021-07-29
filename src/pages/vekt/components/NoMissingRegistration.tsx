f271d87d9e2596c8f263af1a5e2e4be9652cd54f
Adrian Pleym <adrian.pleym@knowit.no>
2021-07-29 09:33:01 +0200

    👌 IMPROVE: Fjernet dummy-data og lagt inn støtte for riktig registreringsdato

--- src/pages/vekt/components/NoMissingRegistration.tsx
+++ src/pages/vekt/components/NoMissingRegistration.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 import { colors } from '../../../theme/foundations/colors';
-import { Heading, HStack, Tbody, Td, Text, Tr, VStack } from '@chakra-ui/react';
-import { DetailWithIcon } from '../../henting/components/DetailWithIcon';
-import Location from '../../../assets/Location.svg';
-import Calendar from '../../../assets/Calendar.svg';
+import { Heading, HStack, Tbody, Td, Text, Tr } from '@chakra-ui/react';
 import { HentingButton } from './HentingButton';
-import { KunTotalvekt } from './KunTotalvekt';
 import { ApiHenting } from '../../../services/henting/HentingService';
 import { KategorierMedVekt } from './KategorierMedVekt';
-import { getDayString } from '../../henting/HentingDetails';
-import { parseISOIgnoreTimezone } from '../../../utils/hentingDateTimeHelpers';
-import { formatTime } from '../../../utils/formatDateTime';
-import { KategoriList } from '../../../components/KategoriList';
 import { EkstraHentingInfo } from '../../ekstrahenting/EkstraHentingInfo';
+import { formatDate } from '../../../utils/formatDateTime';
+import { parseISOIgnoreTimezone } from '../../../utils/hentingDateTimeHelpers';
 
 interface Props {
     henting: ApiHenting;
@@ -33,9 +27,14 @@ export const NoMissingRegistration: React.FC<Props> = ({ henting }) => {
                             <Heading fontSize="1.5rem" fontWeight="bold" textAlign="center">
                                 {henting.vektregistreringer.reduce((total, next) => total + next.vekt, 0)} kg
                             </Heading>
-                            <Text fontSize="0.8rem" fontWeight="normal">
-                                Registrert 20.12.12
-                            </Text>
+                            {henting.vektregistreringer.length > 0 ? (
+                                <Text fontSize="0.8rem" fontWeight="normal">
+                                    Registrert:{' '}
+                                    {formatDate(
+                                        parseISOIgnoreTimezone(henting.vektregistreringer[0].registreringsDato),
+                                    )}
+                                </Text>
+                            ) : null}
                         </HStack>
                     </Td>
 
