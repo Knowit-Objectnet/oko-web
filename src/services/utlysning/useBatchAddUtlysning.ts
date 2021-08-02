64fc81aae25162fb67c388f7fe7e628d56068fd5
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-08-02 11:47:01 +0200

    Fiks for bug OKO-835

--- src/services/utlysning/useBatchAddUtlysning.ts
+++ src/services/utlysning/useBatchAddUtlysning.ts
@@ -2,9 +2,11 @@ import { UseMutationResult } from 'react-query';
 import { useMutationWithInvalidation } from '../useMutationWithInvalidation';
 import { ApiError } from '../httpClient';
 import { ApiUtlysning, ApiUtlysningBatchPost, postBatchUtlysning, utlysningDefaultQueryKey } from './UtlysningService';
+import { ekstraHentingDefaultQueryKey } from '../henting/EkstraHentingService';
+import { hentingDefaultQueryKey } from '../henting/HentingService';
 
-export const useBatchAddUtlysning = (): UseMutationResult<ApiUtlysning[], ApiError, ApiUtlysningBatchPost> =>
-    useMutationWithInvalidation<ApiUtlysning[], ApiError, ApiUtlysningBatchPost>(
+export const useBatchAddUtlysning = (): UseMutationResult<Array<ApiUtlysning>, ApiError, ApiUtlysningBatchPost> =>
+    useMutationWithInvalidation<Array<ApiUtlysning>, ApiError, ApiUtlysningBatchPost>(
         (newUtlysninger) => postBatchUtlysning(newUtlysninger),
-        [utlysningDefaultQueryKey],
+        [utlysningDefaultQueryKey, ekstraHentingDefaultQueryKey, hentingDefaultQueryKey],
     );
