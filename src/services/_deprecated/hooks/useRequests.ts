c2b2cd1bce15f1585e3c65bcda5f5487562a893d
Thomas B. Aschim <thomas.aschim@knowit.no>
2021-08-03 10:08:20 +0200

    Fjernet deprecated kode

--- src/services/_deprecated/hooks/useRequests.ts
+++
@@ -1,9 +0,0 @@
-import { QueryObserverResult, useQuery } from 'react-query';
-import { ApiRequest, ApiRequestParams, getRequests, requestsDefaultQueryKey } from '../RequestService';
-
-export const useRequests = (params: ApiRequestParams = {}): QueryObserverResult<Array<ApiRequest>> => {
-    return useQuery<Array<ApiRequest>>({
-        queryKey: [requestsDefaultQueryKey, params],
-        queryFn: () => getRequests(params),
-    });
-};
--- src/utils/createNDaysFromDate.ts
+++
@@ -1,4 +0,0 @@
-import addDays from 'date-fns/addDays';
-
-export const createNDaysFromDate = (initialDate: Date, numberOfDays: number): Array<Date> =>
-    [...Array(numberOfDays).keys()].map((day) => addDays(initialDate, day));
