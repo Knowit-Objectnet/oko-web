3277a8b0732a00f04178ea74a279d34b9012b425
Lars Kåre Syversen <lars-kare.syversen@knowit.no>
2021-08-23 15:54:39 +0200

    Ny side for personvern. Aksessert gjennom kalender-side, innstillinger og mine avtaler.

--- src/pages/kalender/index.tsx
+++ src/pages/kalender/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { CalendarComponent } from './components/CalendarComponent';
-import { Stack } from '@chakra-ui/react';
+import { Link, Stack } from '@chakra-ui/react';
 import { CalendarProvider } from './CalendarProvider';
 import { CalendarDatePicker } from './components/CalendarDatePicker';
 import { CalendarFilters } from './components/CalendarFilters';
+import { NavLink } from 'react-router-dom';
 
 const Kalender: React.FC = () => (
     <CalendarProvider>
@@ -24,8 +25,24 @@ const Kalender: React.FC = () => (
             <Stack direction={{ base: 'row', tablet: 'column' }} spacing="2" width={{ base: 'full', tablet: 'auto' }}>
                 <CalendarDatePicker />
                 <CalendarFilters />
+                <Link
+                    as={NavLink}
+                    to="/personvern"
+                    textDecorationLine="underline"
+                    display={{ base: 'none', tablet: 'flex' }}
+                >
+                    Om personvern i tjenesten
+                </Link>
             </Stack>
             <CalendarComponent />
+            <Link
+                as={NavLink}
+                to="/personvern"
+                textDecorationLine="underline"
+                display={{ base: 'box', tablet: 'none' }}
+            >
+                Om personvern i tjenesten
+            </Link>
         </Stack>
     </CalendarProvider>
 );
