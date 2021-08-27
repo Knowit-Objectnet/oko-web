1a9c4906f9c45e1b6d0a15e3478a0a21859369b7
Henrik Aspenes Vedal <henrikvedal@Henriks-MacBook-Pro.local>
2021-08-27 10:46:40 +0200

    Justerte storrelse og utheving paa tekst i vektregistrering.

--- src/pages/vekt/registrering/Vektregistrering.tsx
+++ src/pages/vekt/registrering/Vektregistrering.tsx
@@ -62,10 +62,10 @@ export const Vektregistrering: React.FC<Props> = ({ hentingId, label }) => {
                                 {label}
                             </Heading>
                             <Totalvekt vektObjects={vektObjects} />
-                            <Text fontSize="0.75rem" fontWeight={400} maxWidth={420}>
+                            <Text fontSize={{ base: '1rem', desktop: '1.25rem ' }} fontWeight={400} maxWidth={420}>
                                 Byggevarer og materialer, sport- og fritidsutstyr, sykler, tekstiler og hvitevarer skal
                                 registeres under egne vektkategorier. Alle andre varer skal registreres på
-                                <span style={{ fontWeight: 500 }}> Andre ombruksvarer.</span>
+                                <span style={{ fontWeight: 600 }}> Andre ombruksvarer.</span>
                             </Text>
                             <VektForm henting={veiHenting} setVekt={setVekt} onSuccess={onSuccess} />
                         </VStack>
