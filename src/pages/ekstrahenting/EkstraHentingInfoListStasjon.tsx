ba3d26af5a9fefd82ad4b7ed3f32a598354febfa
Oda Skoglund <oda.skoglund@knowit.no>
2021-09-10 09:52:38 +0200

    la til tidligere ekstrahentinger accordion paa stasjonbruker, med ekstrahentinger fra tidligere i naavaerende uke

--- src/pages/ekstrahenting/EkstraHentingInfoListStasjon.tsx
+++ src/pages/ekstrahenting/EkstraHentingInfoListStasjon.tsx
@@ -1,9 +1,11 @@
-import { startOfToday } from 'date-fns';
+import { Accordion } from '@chakra-ui/react';
+import { isFuture, isPast, startOfToday, startOfWeek } from 'date-fns';
 import React from 'react';
 import { useEkstraHentingerWithUtlysning } from '../../services/henting/useEkstraHentingerWithUtlysning';
-import { dateTimeToStringIgnoreTimezone } from '../../utils/hentingDateTimeHelpers';
+import { dateTimeToStringIgnoreTimezone, parseISOIgnoreTimezone } from '../../utils/hentingDateTimeHelpers';
 import { EkstraHentingHeading } from './EkstraHentingHeading';
 import { sortedEkstraHentingerByDatoDesc } from './EkstraHentingSortedInfo';
+import { EkstraHentingAccordion } from './EkstraHentingAccordion';
 
 export const EkstraHentingInfoListStasjon: React.FC = () => {
     const {
@@ -11,18 +13,35 @@ export const EkstraHentingInfoListStasjon: React.FC = () => {
         isLoading,
         isError,
     } = useEkstraHentingerWithUtlysning({
-        after: dateTimeToStringIgnoreTimezone(startOfToday()),
+        after: dateTimeToStringIgnoreTimezone(startOfWeek(startOfToday())),
     });
 
     const sortedEkstraHentinger = sortedEkstraHentingerByDatoDesc(ekstraHentinger ?? []);
 
+    const aktiveEkstrahentinger = sortedEkstraHentinger.filter((ekstraHentinger) =>
+        isFuture(parseISOIgnoreTimezone(ekstraHentinger.sluttTidspunkt)),
+    );
+
+    const sortedTidligereEkstraHentinger = sortedEkstraHentinger.filter((ekstraHentinger) =>
+        isPast(parseISOIgnoreTimezone(ekstraHentinger.sluttTidspunkt)),
+    );
+
     return (
-        <EkstraHentingHeading
-            ekstraHentinger={sortedEkstraHentinger}
-            label="Aktive ekstrahentinger"
-            isLoading={isLoading}
-            isError={isError}
-            isPast={false}
-        />
+        <Accordion allowToggle borderColor="transparent" allowMultiple>
+            <EkstraHentingHeading
+                ekstraHentinger={aktiveEkstrahentinger}
+                label="Aktive ekstrahentinger"
+                isLoading={isLoading}
+                isError={isError}
+                isPast={false}
+            />
+            <EkstraHentingAccordion
+                ekstraHentinger={sortedTidligereEkstraHentinger}
+                label="Tidligere ekstrahentinger"
+                isLoading={isLoading}
+                isError={isError}
+                isPast={true}
+            />
+        </Accordion>
     );
 };
